7ed91e1cf4123de35670850e0babb58ce2cd9ee0
Edson Galdino <edsongaldino@outlook.com>
2023-05-12 11:02:00 -0400

    Publicação página de receitas - Atividade FrontEnd

---
+++ assets/js/paginar.js
@@ -0,0 +1,20 @@
+function Page1()
+{
+    document.getElementById("pag1").style.display = "block";
+    document.getElementById("pag2").style.display = "none";
+    document.getElementById("pag3").style.display = "none";
+}
+
+function Page2()
+{
+    document.getElementById("pag1").style.display = "none";
+    document.getElementById("pag2").style.display = "block";
+    document.getElementById("pag3").style.display = "none";
+}
+
+function Page3()
+{
+    document.getElementById("pag1").style.display = "none";
+    document.getElementById("pag2").style.display = "none";
+    document.getElementById("pag3").style.display = "block";
+}
